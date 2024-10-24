6bdf62a9e91743ca469f24f399c5bd4d465a4603
jinhong <jinhong.gao@mail.utoronto.ca>
2024-10-23 22:07:36 -0400

    Task 2.2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -32,4 +32,10 @@ public interface LoginUserDataAccessInterface {
      * @param name The name of the user.
      */
     void setCurrentUser(String name);
+
+    /**
+     * Gets the current user.
+     * @return the username of the current logged-in user.
+     */
+    String getCurrentUser();
 }
